19fca3881fc1f92cf17ba1982cf85f7face69642
Roei Boribor <roeiboribor@gmail.com>
2024-04-11 07:21:44 +0800

    major changes

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro, soluta!
+		</main>
+	);
+}
