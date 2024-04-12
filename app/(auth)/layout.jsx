cd694f684f2d9cad82a5118460538e4b6390cea3
Roei Boribor <roeiboribor@gmail.com>
2024-04-12 15:43:26 +0800

    change layout

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -8,18 +8,16 @@ export const metadata = {
 
 const Layout = ({ children }) => {
 	return (
-		<main>
-			<div className="font-sans text-gray-900 antialiased">
-				<AuthCard
-					logo={
-						<Link href="/">
-							<ApplicationLogo className="h-20 fill-current text-gray-500" />
-						</Link>
-					}
-				>
-					{children}
-				</AuthCard>
-			</div>
+		<main className="font-sans text-gray-900 antialiased">
+			<AuthCard
+				logo={
+					<Link href="/">
+						<ApplicationLogo className="h-20 fill-current text-gray-500" />
+					</Link>
+				}
+			>
+				{children}
+			</AuthCard>
 		</main>
 	);
 };
